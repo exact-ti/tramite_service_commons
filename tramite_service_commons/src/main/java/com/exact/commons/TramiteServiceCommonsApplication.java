8c14c857dc96b9359f4a3a9d06c021004ee96fd9
oheredia <oheredia@exact.com.pe>
2020-05-08 09:28:43 -0500

    Actualización

--- tramite_service_commons/src/main/java/com/exact/commons/TramiteServiceCommonsApplication.java
+++ tramite_service_commons/src/main/java/com/exact/commons/TramiteServiceCommonsApplication.java
@@ -1,13 +1,11 @@
 package com.exact.commons;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class TramiteServiceCommonsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TramiteServiceCommonsApplication.class, args);
-	}
 
 }
